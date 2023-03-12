8470a60129f760f2f38667eaec33af987324f141
pepe32321 <108217611+pepe32321@users.noreply.github.com>
2023-03-12 20:09:03 +0100

    liars_poker_v5.py
    completed implementation of a game

--- liars_poker.py
+++ liars_poker.py
@@ -48,8 +48,7 @@ class Deck:
 		#print(f'going to remove {possible_num} cards')
 		cards=self.cards[-possible_num:]
 		self.cards=self.cards[:-possible_num]
-		global cards_on_table
-		cards_on_table+=[str(card) for card in cards]
+		status.cards_on_table+=[str(card) for card in cards]
 		return cards
 
 	# deals one card
@@ -65,310 +64,329 @@ class Deck:
 		random.shuffle(self.cards)
 		print('deck shuffled')
 
+	def _hierarchy(self):
+
+		# lists of possibles poker figures
+
+		# auxiliary variable to define figures
+		values_perm=[list(comb) for comb in (permutations(self.values, 2))]
+
+		high_cards_keys=[f'high card {value}' for value in self.values]
+
+		pairs_keys=[f'pair of {value}' for value in self.values]
+
+		pairs_keys.reverse()
+
+		two_pairs_keys =[' '.join(list(two)) for two in list(combinations(pairs_keys, 2))]
+		two_pairs_keys.reverse()
+		pairs_keys.reverse()
+
+		three_of_a_kind_keys=[f'three of {value}' for value in self.values]
+
+		three_of_a_kind_keys.reverse()
+
+		double_three_of_a_kind_keys =[' '.join(list(three)) for three in list(combinations(three_of_a_kind_keys, 2))]
+		double_three_of_a_kind_keys.reverse()
+		three_of_a_kind_keys.reverse()
+
+		full_house_keys=[f'three of {values_perm[i][0]} pair of {values_perm[i][1]}' for i in range(len(values_perm))]
+
+		four_of_a_kind_keys=[f'four of {value}' for value in self.values]	
+
+		poker_keys=[f'poker of {suit}' for suit in self.suits]
+
+		#full list of possible  poker figure names
+		return ['None']+high_cards_keys+pairs_keys+two_pairs_keys+three_of_a_kind_keys+full_house_keys+double_three_of_a_kind_keys+four_of_a_kind_keys+poker_keys
+		
+
 
 # player has specified name and hand_size
 class Player:
 	
 	# by default player has 1 card on hand
 	# each player has a list of card on his hand
-	def __init__(self,name,hand_size=1):
+	def __init__(self,name,hand_size=1,strategy=None):
 		self.name=name
+		self.def_hand_size=hand_size
 		self.hand_size=hand_size
 		self.cards_on_hand=[]
+		self.strategy=strategy
+		self.possible_cards=[]
 
 	def __repr__(self):
 		return f'{self.name}'
 
 	# each player can  bid a poker figure which is higher than global actual_bid variable
 	# bidding is made by random choice of position in sliced list of possible figures
-	def _bid(self):
-		global actual_bid 
-		actual_bid=random.choice(list(possible_figures.keys())[list(possible_figures.keys()).index(actual_bid)+1:])
-		print(f'{self.name}: There are {actual_bid} ')
+	def _bid(self): 
+		print(status.actual_bid)
+		print(str(self.cards_on_hand[0])[:-1])
+		print(status.cards_declared)
+		self.possible_cards=[str(card)[:-1] for card in self.cards_on_hand]
+		print(self.possible_cards)
+		self.possible_cards.extend(status.cards_declared)
+		print(self.possible_cards)
+		
+
+		if status.actual_bid=='None':
+			status.actual_bid=f'high card {str(random.choice(self.cards_on_hand))[:-1]}'
+		else:
+			i=0
+			for possible_figure in list(status.possible_figures[list(status.possible_figures).index(status.actual_bid)+1:]):
+				if len(possible_figure.split())==3:
+					if self.possible_cards.count(possible_figure.split()[2])>1:
+						status.actual_bid=possible_figure
+						i=1
+						break
+				elif len(possible_figure.split())==6:
+					if self.possible_cards.count(possible_figure.split()[2])>1 & self.possible_cards.count(possible_figure.split()[5])>1:
+						status.actual_bid=possible_figure
+						i=1
+						break
+			if i==0:
+				status.actual_bid=random.choice(list(status.possible_figures[list(status.possible_figures).index(status.actual_bid)+1:list(status.possible_figures).index(status.actual_bid)+6]))
+
+
+
+		if len(status.actual_bid.split())==3:
+			status.cards_declared.append(status.actual_bid.split()[2])
+		elif len(status.actual_bid.split())==6:
+			status.cards_declared.extend([status.actual_bid.split()[2],status.actual_bid.split()[5]])
+		print(status.cards_declared)
+		print(f'{self.name}: There are {status.actual_bid} ')
 
 	# instead of bidding player can check if previous player's bid was True
 	def _check(self):
-		global check
-		global checkin_player
-		check=True
-		checkin_player=players.index(self)
+		status.check=True
+		status.checkin_player=status.active_players.players_list.index(self)
 		print(f'{self.name}: Check')
 
 	# each player decides if bid or check by random decision
-	def _decide(self):
-		global actual_bid
+	def _default_strategy(self):
 		decision=random.randint(0,1)
 		#print(decision)
-		if 'poker' in actual_bid or decision==1:
+		if status.actual_bid!='None' and ('poker' in status.actual_bid or decision==1):
 			#print('_check')
 			return self._check()
-		if decision==0 or actual_bid=='None':
+		if decision==0  :
 			#print('_bid')
 			return self._bid() 
-			
+
+	# each player decides if bid or check by his strategy or by self._deafult_strategy()
+	def _decide(self):
+		if self.strategy:
+			return self.strategy(self)
+		return self._default_strategy()
+
+	# function returns True if chcecked person was right
+	def checking(self):
+		if 'high' in status.actual_bid:
+			if status.actual_bid.split()[2] in ''.join(status.cards_on_table):
+				return True
+			else:
+				return False
+
+		elif status.actual_bid.count('pair')==1 and status.actual_bid.count('three')==0 :
+			if ''.join(status.cards_on_table).count(status.actual_bid.split()[2])>1:
+				return True
+			else:
+				return False
+
+		elif status.actual_bid.count('pair')==2 :
+			if ''.join(status.cards_on_table).count(status.actual_bid.split()[2])>1 and ''.join(status.cards_on_table).count(status.actual_bid.split()[5])>1 :
+				return True
+			else:
+				return False
+
+		elif status.actual_bid.count('three')==0 and status.actual_bid.count('pair')==1 :
+			if ''.join(status.cards_on_table).count(status.actual_bid.split()[2])>2:
+				return True
+			else:
+				return False
+
+		elif status.actual_bid.count('three')==1 and status.actual_bid.count('pair')==1 :
+			if ''.join(status.cards_on_table).count(status.actual_bid.split()[2])>2 and ''.join(status.cards_on_table).count(status.actual_bid.split()[5])>1:
+				return True
+			else:
+				return False
+
+		elif status.actual_bid.count('three')==2 :
+			if ''.join(status.cards_on_table).count(status.actual_bid.split()[2])>2 and ''.join(status.cards_on_table).count(status.actual_bid.split()[5])>2:
+				return True
+			else:
+				return False
 
 
-"""
-Creation of dictionary of possible poker figures in the game-------------------------------------------------------------------
-will be moved to deck class
-"""
+		elif status.actual_bid.count('four')==1 :
+			if ''.join(status.cards_on_table).count(status.actual_bid.split()[2])>3 :
+				return True
+			else:
+				return False
 
-def _hierarchy():
 
-	# lists of possibles thai poker figures
-
-	# auxiliary variable to define figures
-	values_perm=[list(comb) for comb in (permutations(deck.values, 2))]
-	#print(values_perm)
-
-	high_cards_keys=[f'high card {value}' for value in deck.values]
-	high_cards_values=[value for value in deck.values]
-
-	pairs_keys=[f'pair of {value}' for value in deck.values]
-	pairs_values=[value for value in deck.values]
-
-	#sorted!!!
-
-	# print(pairs_keys)
-	rev_pair_keys=pairs_keys
-	rev_pair_keys.reverse()
-	# print(rev_pair_keys)
-
-	# print(pairs_keys)
-	rev_pair_values=pairs_values
-	rev_pair_values.reverse()
-	# print(rev_pair_values)
-
-	two_pairs_keys =[' '.join(list(two)) for two in list(combinations(rev_pair_keys, 2))]
-	two_pairs_keys.reverse()
-	two_pairs_values=[(list(two)) for two in list(combinations(rev_pair_values, 2))]
-	two_pairs_values.reverse()
-
-	three_of_a_kind_keys=[f'three of {value}' for value in deck.values]
-	three_of_a_kind_values=[value for value in deck.values]
-
-	#sorted!!!
-
-	# print(three_of_a_kind_keys)
-	rev_three_key=three_of_a_kind_keys
-	rev_three_key.reverse()
-	# print(rev_three_key)
-
-	# print(three_of_a_kind_values)
-	rev_three_values=three_of_a_kind_values
-	rev_three_values.reverse()
-	# print(rev_three_values)
-
-	double_three_of_a_kind_keys =[' '.join(list(three)) for three in list(combinations(rev_three_key, 2))]
-	double_three_of_a_kind_keys.reverse()
-	double_three_of_a_kind_values =[(list(three)) for three in list(combinations(rev_three_values, 2))]
-	double_three_of_a_kind_values.reverse()
-
-	full_house_keys=[f'three of {values_perm[i][0]} pair of {values_perm[i][1]}' for i in range(len(values_perm))]
-	full_house_values=values_perm
-
-	four_of_a_kind_keys=[f'four of {value}' for value in deck.values]
-	four_of_a_kind_values=[value for value in deck.values]
-
-	poker_keys=[f'poker of {suit}' for suit in deck.suits]
-	poker_values=[[value+suit for value in ['9','10','J','Q','K','A']] for suit in deck.suits]
-	#[suit for suit in deck.suits]
-
-	#full list of possible thai poker figure names
-	possible_figure_keys=['None']+high_cards_keys+pairs_keys+two_pairs_keys+three_of_a_kind_keys+full_house_keys+double_three_of_a_kind_keys+four_of_a_kind_keys+poker_keys
-	possible_figure_values=['None']+high_cards_values+pairs_values+two_pairs_values+three_of_a_kind_values+full_house_values+double_three_of_a_kind_values+four_of_a_kind_values+poker_values
-	return {possible_figure_keys[i]:possible_figure_values[i] for i in range(0,len(possible_figure_keys))}
+		elif status.actual_bid.count('poker')==1 :
+			poker_values=[[value+suit for value in ['9','10','J','Q','K','A']] for suit in status.deck.suits]
+			if  any([set(value).issubset(set(status.cards_on_table)) for value in poker_values]):
+				return True
+			else:
+				return False	
 	
 
 
-	# auxiliary prints
+class Players:
+	def __init__(self,def_players):
+		self.def_players=def_players
+		self.players_list=def_players
+		self.players_names=[player.name for player in self.players_list]
+		self.players_stat={player.name:0 for player in self.def_players}
+		
+	def __repr__(self):
+		return f"Players: {self.players_names}"
 
-	"""
+	def _refresh(self):
+		self.players_list=self.def_players
+		self.players_names=[player.name for player in self.players_list]
+		for player in self.players_list:
+			player.hand_size=player.def_hand_size
+			player.cards_on_hand=[]
 
-	print(high_cards_keys)
-	print(high_cards_values)
-	print('  ')
-
-	print(pairs_keys)
-	print(pairs_values)
-	print('  ')
-
-	print(two_pairs_keys)
-	print(two_pairs_values)
-	print('  ')
-
-	print(double_three_of_a_kind_keys)
-	print(double_three_of_a_kind_values)
-	print('  ')
-
-	print(three_of_a_kind_keys)
-	print(three_of_a_kind_keys)
-	print('  ')
-
-	print(full_house_keys)
-	print(full_house_keys)
-	print('  ')
-
-	print(four_of_a_kind_keys)
-	print(four_of_a_kind_values)
-	print('  ')
-
-	print(poker_keys)
-	print(poker_values)
-	print('  ')
-
-	#print(possible_figure_keys)
-	#print(possible_figure_values)
-
-	"""
-
-# wil be moved to Player class	
-def checking():
-
-	#print(''.join(cards_on_table))
-	#print(''.join(cards_on_table).count(actual_bid.split()[2]))
+	def show_stats(self):
+		for key,value in self.players_stat.items():
+			print(key,value)
 
 
-	if 'high' in actual_bid:
-		#print('high')
-		if actual_bid.split()[2] in ''.join(cards_on_table):
-			return True
+
+# stores variables used by other classes through the game
+class Game_status:
+	def __init__(self,def_players):
+		# initial conditions
+		self.active_players=def_players
+		self.possible_figures=[]
+		self.actual_bid='None'
+		self.check=False
+		self.checkin_player=None
+		self.cards_on_table=[]
+		self.deck=[]
+		self.cards_declared=[]
+	
+	def _refresh(self):
+		self.actual_bid='None'
+		self.check=False
+		self.checkin_player=None
+		self.cards_on_table=[]
+		self.deck=[]
+		self.cards_declared=[]
+
+
+
+# game contains of its number, initial conditions and run method which initialize gameplay
+class Game:
+	def __init__(self,number):
+		self.number=number
+		winner=None
+
+	def __repr__(self):
+		return f"Game no. {self.number}"
+
+	def _run(self):
+		print(self.__repr__())
+		# main loop of the game
+		while len(status.active_players.players_list)>1:
+
+			# declare a deck
+			# for four players with one card on their hands it's common to limit deck values
+			status.deck=Deck(['8','9','10','J','Q','K','A'])
+			status.possible_figures=status.deck._hierarchy()
+
+			# shuffle a deck
+			status.deck.shuffle()
+			for player in status.active_players.players_list:
+				player.cards_on_hand+=status.deck.deal_hand(player.hand_size)
+				print(f'{player.name} has {player.cards_on_hand} on hand')
+
+			print(f'cards on  a table: {status.cards_on_table}')
+
+			# a loop of players bidding ended by check
+			while status.check==False:
+				for player in status.active_players.players_list:
+					player._decide()
+					if status.check:
+						break
+
+			# result of check
+			print(f'cards on  a table: {status.cards_on_table}')
+			if player.checking():
+				status.active_players.players_list[status.checkin_player].hand_size+=1
+				print(f'{status.active_players.players_list[status.checkin_player].name} loses')
+			else:
+				status.active_players.players_list[status.checkin_player-1].hand_size+=1
+				print(f'{status.active_players.players_list[status.checkin_player-1].name} loses')
+
+			# new split
+			for player in status.active_players.players_list:
+				player.cards_on_hand=[]
+			status.active_players.players_list=[player for player in status.active_players.players_list if player.hand_size<6]
+			status.active_players.players_names=[player.name for player in status.active_players.players_list]
+			status._refresh()
+			print(players.players_names)
+
+		self.winner=status.active_players.players_list[0].name
+		print(f'the winner is {status.active_players.players_names[0]} with {status.active_players.players_list[0].hand_size} cards on hand')
+		players.players_stat[self.winner]+=1
+		players._refresh()
+		status.__init__(players)
+		return 
+
+
+# Strategies-----------------------------------------------------------------------------------------------------------------
+
+
+# first strategy
+def first_strategy(self):
+		self.possible_cards.extend(status.cards_declared)
+		if status.actual_bid=='None':
+			# print(status.actual_bid)
+			# print(self.cards_on_hand)
+			# print(list(status.possible_figures[list(status.possible_figures).index(status.actual_bid)+1:]))
+			# print(status.possible_figures[1:len(status.deck.values)])
+			status.actual_bid=random.choice(list(status.possible_figures[1:len(status.deck.values)+1]))
+			status.cards_declared.append(status.actual_bid.split()[2])
+			# print(status.cards_declared)
+			print(f'{self.name}: There are {status.actual_bid} ')
+		elif len(status.actual_bid.split())==3:
+			if self.possible_cards.count(status.actual_bid.split()[2])>1:
+				return self._bid()
+		elif len(status.actual_bid.split())==6:
+			if self.possible_cards.count(status.actual_bid.split()[2])>1 & self.possible_cards.count(status.actual_bid.split()[5])>1:
+				return self._bid()
 		else:
-			return False
-
-	elif actual_bid.count('pair')==1 and actual_bid.count('three')==0 :
-		#print('pair')
-		if ''.join(cards_on_table).count(actual_bid.split()[2])>1:
-			return True
-		else:
-			return False
-
-	elif actual_bid.count('pair')==2 :
-		#print('two pairs')
-		if ''.join(cards_on_table).count(actual_bid.split()[2])>1 and ''.join(cards_on_table).count(actual_bid.split()[5])>1 :
-			return True
-		else:
-			return False
-
-	elif actual_bid.count('three')==0 and actual_bid.count('pair')==1 :
-		#print('three')
-		if ''.join(cards_on_table).count(actual_bid.split()[2])>2:
-			return True
-		else:
-			return False
-
-	elif actual_bid.count('three')==1 and actual_bid.count('pair')==1 :
-		#print('full house')
-		if ''.join(cards_on_table).count(actual_bid.split()[2])>2 and ''.join(cards_on_table).count(actual_bid.split()[5])>1:
-			return True
-		else:
-			return False
-
-	elif actual_bid.count('three')==2 :
-		#print('two threes')
-		if ''.join(cards_on_table).count(actual_bid.split()[2])>2 and ''.join(cards_on_table).count(actual_bid.split()[5])>2:
-			return True
-		else:
-			return False
-
-
-	elif actual_bid.count('four')==1 :
-		#print('four')
-		if ''.join(cards_on_table).count(actual_bid.split()[2])>3 :
-			return True
-		else:
-			return False
-
-
-	elif actual_bid.count('poker')==1 :
-		#print('poker')
-		#print(possible_figures.get(actual_bid))
-		if   set(possible_figures.get(actual_bid)).issubset(set(cards_on_table)):
-			return True
-		else:
-			return False
-
+			return self._check()
 
 
 # Game begins---------------------------------------------------------------------------------------------------------------
 
 
 # define players
-players=[
-Player('Paweł'),
+players=Players([
+Player('Paweł',1,first_strategy),
 Player('Adi'),
 Player('Michał'),
 Player('Daro')
-]
-players_names=[player.name for player in players]
-print(players_names)
+])
 
-# main loop of the game
-while len(players)>1:
+status=Game_status(players)
 
-	# declare a deck
-	# for four players with one card on their hands it's common to limit deck values
-	deck=Deck(['8','9','10','J','Q','K','A'])
-	possible_figures=_hierarchy()
-	#print(list(possible_figures.keys()))
-	#print(deck)
-	#print(deck.cards)
+game_one=Game(1)
+game_one._run()
 
-	# initial global conditions
-	actual_bid='None'
-	check=False
-	checkin_player=None
-	cards_on_table=[]
+game_two=Game(2)
+game_two._run()	
 
-	# shuffle a deck
-	deck.shuffle()
-	for player in players:
-		player.cards_on_hand+=deck.deal_hand(player.hand_size)
-		print(f'{player.name} has {player.cards_on_hand} on hand')
-
-	print(f'cards on  a table: {cards_on_table}')
-
-	# a loop of players bidding ended by check
-	while check==False:
-		for player in players:
-			player._decide()
-			if check:
-				break
-
-	
-
-	# result of check
-	#print(f'checked bid: {actual_bid}')
-	print(f'cards on  a table: {cards_on_table}')
-	if checking():
-		players[checkin_player].hand_size+=1
-		print(f'{players[checkin_player].name} loses')
-	else:
-		players[checkin_player-1].hand_size+=1
-		print(f'{players[checkin_player-1].name} loses')
-
-	# new split
-	for player in players:
-		player.cards_on_hand=[]
-	players=[player for player in players if player.hand_size<5]
-	players_names=[player.name for player in players]
-	print(players_names)
-
-print(f'the winner is {players_names[0]} with {players[0].hand_size} cards on hand')
+players.show_stats()
 
 
+for i in range(100):
+	game_loop=Game(i)
+	game_loop._run()
+players.show_stats()
 
-# auxiliary checking test
-
-"""
-
-for i in range(1,len(possible_figure_keys)):
-	actual_bid=possible_figure_keys[i]
-	print(actual_bid)
-	print(actual_bid.split())
-	if checking():
-		print ('true')
-	else:
-		print ('false')
-
-"""
